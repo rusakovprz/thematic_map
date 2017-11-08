8ac8a2dc91935bf6493674ec4c5b41d8bd402f34
Rusakov Alexey <rusakovprx@rambler.ru>
2017-11-08 21:57:45 +0300

    Проведено раздиление утилиты на Lib and GUI. ref #5

---
+++ thematic_map_lib.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+import csv
+import cairo
+import rsvg
+from xml.dom import minidom
+import xml.etree as etree
+
+
+global colors
+
+colors = [
+[
+# 4 градации серого (gray)
+[[247, 247, 247],[204, 204, 204],[150, 150, 150],[82, 82, 82]],
+# 5 градаций серого
+[[247, 247, 247],[204, 204, 204],[150, 150, 150],[99, 99, 99],[37, 37, 37]],
+# 6 градаций серого
+[[247, 247, 247],[217, 217, 217],[189, 189, 189],[150, 150, 150],[99, 99, 99],[37, 37, 37]]
+],
+#----------------------------------------
+[
+# 4 градации оранжевого (orange)
+[[254, 240, 217],[253, 204, 138],[252, 141, 89],[215, 48, 31]],
+# 5 градаций оранжевого
+[[254, 240, 217],[253, 204, 138],[252, 141, 89],[227, 74, 51],[179, 0, 0]],
+# 6 градаций оранжевого
+[[254, 240, 217],[253, 212, 158],[253, 187, 132],[252, 141, 89],[227, 74, 51],[179, 0, 0]]
+],
+#----------------------------------------
+[
+# 4 градации фиолетового (purple)
+[[242, 240, 247],[203, 201, 226],[158, 154, 200],[106, 81, 163]],
+# 5 градаций фиолетового
+[[242, 240, 247],[203, 201, 226],[158, 154, 200],[117, 107, 177],[84, 39, 143]],
+# 6 градаций фиолетового
+[[242, 240, 247],[218, 218, 235],[188, 189, 220],[158, 154, 200],[117, 107, 177],[84, 39, 143]]
+],
+#----------------------------------------
+[
+# 4 градации зелёного
+[[237, 248, 233],[186, 228, 179],[116, 196, 118],[35, 139, 69]],
+# 5 градаций зелёного
+[[237, 248, 233],[186, 228, 179],[116, 196, 118],[49, 163, 84],[0, 109, 44]],
+# 6 градаций зелёного
+[[237, 248, 233],[199, 233, 192],[161, 217, 155],[116, 196, 118],[49, 163, 84],[0, 109, 44]]
+]		]
+
+#=======================================================================================================================
+
+def read_and_parse_csv(filename):
+  data = {}
+
+  try:
+    reader = csv.reader(open(filename), delimiter=",")
+  except IOError:
+    showerror("Ошибка", "Указанный файл не найден.")
+    return data
+
+  for row in reader:
+    try:
+      region = row[0]
+      value =  row[1].strip()
+      data[region] = value
+
+    except IndexError as err:
+      print "read_and_parse_csv Error:", err
+
+    except :
+      showerror("Ошибка", "Нарушена стркутура CSV файла.")
+
+  return data
+
+#=======================================================================================================================  
+
+def get_min_max_values(in_value):
+
+  tmp_array = []
+
+  i = 0
+  while i < len(in_value):
+    try:
+      tmp_array.append( float(in_value[i]) )
+    except:
+      pass
+    i += 1
+
+  return min(tmp_array), max(tmp_array)
+
+#=======================================================================================================================  
+
+def edit_svg(svg, statistic, match_band, bands, palett):
+
+  # Признак отсутствия данных хотябы для одного региона
+  flag_no_data = False
+
+  # Парсим SVG
+  doc = minidom.parseString(svg)
+
+  # просматриваем всю карту
+  # Находим соответствующий узел и редактируем его
+  paths = doc.getElementsByTagName("path")
+
+  for path in paths:
+      region = path.getAttribute("id")
+
+      # В SVG файле могут быть объекты с тегом "path" не опписывающие границы региона
+      if region == "":
+        continue
+
+      try:
+        value = float(statistic[region])
+
+      except:
+        flag_no_data = True
+
+        if palett == 0:
+          path.setAttribute("style", "")
+
+          path.setAttribute("fill", "url(#hatching)")
+          path.setAttribute("stroke", "black")
+          path.setAttribute("stroke-width", "2")
+
+        else:
+          [r,g,b] = colors[0][2][3]
+          style="fill: rgb(" + str(r) + ", " + str(g) + ", " + str(b) + "); fill-opacity: 1;"
+          path.setAttribute("style", style)
+        continue
+
+      else:
+
+        col = 0
+        i = 0
+        while i < match_band:
+          if value >= bands[i][0] and value < bands[i][1]:
+            col = i
+            break
+          i += 1
+
+        [r,g,b] = colors[palett][match_band-4][col]
+        style="fill: rgb(" + str(r) + ", " + str(g) + ", " + str(b) + "); fill-opacity: 1;"
+        path.setAttribute("style", style)
+
+  # Легенда
+  paths = doc.getElementsByTagName("rect")
+
+  for path in paths:
+      ID = path.getAttribute("id")
+      if int(ID) > match_band:
+        if flag_no_data :
+          if palett > 0:
+            [r,g,b] = colors[0][2][3]
+            style="fill: rgb(" + str(r) + ", " + str(g) + ", " + str(b) + "); fill-opacity: 1;"
+            path.setAttribute("style", style)
+          else:
+            path.setAttribute("style", "")
+            path.setAttribute("fill", "url(#hatching)")
+            path.setAttribute("stroke", "black")
+            path.setAttribute("stroke-width", "2")
+        break
+
+      [r,g,b] = colors[palett][match_band-4][int(ID)-1]
+      style="fill: rgb(" + str(r) + ", " + str(g) + ", " + str(b) + "); fill-opacity: 1;"
+      path.setAttribute("style", style)
+
+
+  # подпись легенды
+  paths = doc.getElementsByTagName("text")
+
+  for path in paths:
+      ID = path.getAttribute("id")
+      if int(ID) > match_band:
+        if flag_no_data :
+          path.setAttribute("style", "font-size: 30px; fill: rgb(0, 0, 0); fill-opacity: 1; font-family: Arial;")
+
+          s = unicode( "Значение не задано", encoding='utf-8' )
+          textnode = doc.createTextNode( s )
+          path.appendChild(textnode)
+
+        break
+
+      path.setAttribute("style", "font-size: 30px; fill: rgb(0, 0, 0); fill-opacity: 1; font-family: Arial;")
+      s = unicode( str(bands[ int(ID)-1 ][0]) + " - " + str(bands[int(ID)-1][1]) , encoding='utf-8' ) 
+      textnode = doc.createTextNode( s )
+      path.appendChild(textnode)
+
+  return doc.toprettyxml(encoding='utf-8')
+
